8ce8b7ee9e54ef8a18925226d5f28de9f6922193
hisyam <hisyam@molpay.com>
2021-07-22 14:35:50 +0800

    add all files

---
+++ app/src/main/java/com/molpay/molpayxdkproject/MainActivity.java
@@ -0,0 +1,96 @@
+package com.molpay.molpayxdkproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.molpay.molpayxdk.MOLPayActivity;
+
+import java.util.Calendar;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private void restartmolpay() {
+        HashMap<String, Object> paymentDetails = new HashMap<>();
+        paymentDetails.put(MOLPayActivity.mp_amount, "1.01");
+
+        paymentDetails.put(MOLPayActivity.mp_username, "");
+        paymentDetails.put(MOLPayActivity.mp_password, "");
+        paymentDetails.put(MOLPayActivity.mp_merchant_ID, "");
+        paymentDetails.put(MOLPayActivity.mp_app_name, "");
+        paymentDetails.put(MOLPayActivity.mp_verification_key, "");
+
+        paymentDetails.put(MOLPayActivity.mp_order_ID, Calendar.getInstance().getTimeInMillis());
+        paymentDetails.put(MOLPayActivity.mp_currency, "MYR");
+        paymentDetails.put(MOLPayActivity.mp_country, "MY");
+        paymentDetails.put(MOLPayActivity.mp_channel, "multi");
+        paymentDetails.put(MOLPayActivity.mp_bill_description, "bill description");
+        paymentDetails.put(MOLPayActivity.mp_bill_name, "bill name");
+        paymentDetails.put(MOLPayActivity.mp_bill_email, "example@gmail.com");
+        paymentDetails.put(MOLPayActivity.mp_bill_mobile, "01234567888");
+        paymentDetails.put(MOLPayActivity.mp_channel_editing, false);
+        paymentDetails.put(MOLPayActivity.mp_editing_enabled, true);
+
+        Intent intent = new Intent(MainActivity.this, MOLPayActivity.class);
+        intent.putExtra(MOLPayActivity.MOLPayPaymentDetails, paymentDetails);
+        startActivityForResult(intent, MOLPayActivity.MOLPayXDK);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MOLPayActivity.MOLPayXDK && resultCode == RESULT_OK){
+            Log.d(MOLPayActivity.MOLPAY, "MOLPay result = "+data.getStringExtra(MOLPayActivity.MOLPayTransactionResult));
+            TextView tw = (TextView)findViewById(R.id.resultTV);
+            tw.setText(data.getStringExtra(MOLPayActivity.MOLPayTransactionResult));
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        // closebtn clicked
+        if (id == R.id.newBtn) {
+            restartmolpay();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
