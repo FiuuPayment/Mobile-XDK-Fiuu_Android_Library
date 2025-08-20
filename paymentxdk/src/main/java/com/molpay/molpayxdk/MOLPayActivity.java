de416e5c43db85bd9e555bd1bf6d896356b6d389
haziqruslan27 <haziq.ruslan@fiuu.com>
2025-08-20 16:10:48 +0800

    Implement Webview Core V2 in Android library

--- paymentxdk/src/main/java/com/molpay/molpayxdk/MOLPayActivity.java
+++ paymentxdk/src/main/java/com/molpay/molpayxdk/MOLPayActivity.java
@@ -105,6 +105,7 @@ public class MOLPayActivity extends AppCompatActivity {
     public final static String mp_metadata = "mp_metadata";
     public final static String mp_gpay_channel = "mp_gpay_channel";
     public final static String mp_hide_googlepay = "mp_hide_googlepay";
+    public final static String mp_classic_webcore = "mp_classic_webcore";
     public final static String device_info = "device_info";
 
     public final static String MOLPAY = "logMOLPAY";
@@ -116,7 +117,7 @@ public class MOLPayActivity extends AppCompatActivity {
     private final static String mpclickgpbutton = "mpclickgpbutton://";
     private final static String module_id = "module_id";
     private final static String wrapper_version = "wrapper_version";
-    private final static String wrapperVersion = "17a";
+    private final static String wrapperVersion = "18a";
 
     private String filename;
     private Bitmap imgBitmap;
@@ -127,6 +128,7 @@ public class MOLPayActivity extends AppCompatActivity {
     private Boolean isClosingReceipt = false;
     private Boolean isClosebuttonDisplay = false;
     private Boolean isEnableFullscreen = false;
+    private String setMPMainUI = "";
     private Boolean isTNGResult = false;
 
     private static final Gson gson =  new Gson();
@@ -202,6 +204,13 @@ public class MOLPayActivity extends AppCompatActivity {
                 }
             }
 
+            boolean isClassicWebcore = json.optBoolean("mp_classic_webcore", false);
+
+            setMPMainUI = isClassicWebcore
+                    ? "https://pay.merchant.razer.com/RMS/API/xdk/"
+                    : "https://xdk.fiuu.com/";
+
+
             if (paymentDetails.containsKey("is_submodule")) {
                 is_submodule = Boolean.parseBoolean(Objects.requireNonNull(paymentDetails.get("is_submodule")).toString());
             }
@@ -245,7 +254,7 @@ public class MOLPayActivity extends AppCompatActivity {
         cookieManager.setAcceptThirdPartyCookies(mpMainUI, true);
         cookieManager.setAcceptThirdPartyCookies(mpMOLPayUI, true);
 
-        mpMainUI.loadUrl("https://pay.merchant.razer.com/RMS/API/xdk/");
+        mpMainUI.loadUrl(setMPMainUI);
 
         // Configure MOLPay ui
         mpMOLPayUI.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
